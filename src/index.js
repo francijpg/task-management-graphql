7f55c0ac2cee3a134d1ccbcf5368c91a0fecc36e
francijpg <hellofrancijpg@gmail.com>
2021-05-28 11:03:24 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = require("./db/schema");
+const resolvers = require("./db/resolvers");
+
+const connectDB = require("./config/db");
+
+connectDB();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready in: ${url}`);
+});
