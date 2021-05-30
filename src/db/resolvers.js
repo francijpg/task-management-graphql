13f62bc9e7d5a330425ebc5441b1caa98c835072
francijpg <hellofrancijpg@gmail.com>
2021-05-30 11:32:05 -0400

    add authenticateUser resolver

--- src/db/resolvers.js
+++ src/db/resolvers.js
@@ -1,9 +1,20 @@
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: ".env" });
 
 const User = require("../models/User");
 
+const createToken = (user, secret, expiresIn) => {
+  const { id, email, name } = user;
+  return jwt.sign({ id, email, name }, secret, { expiresIn });
+};
+
 const resolvers = {
-  Query: {},
+  Query: {
+    getProjects: async (_, {}, ctx) => {
+      console.log("getting projects...");
+    },
+  },
   Mutation: {
     createUser: async (_, { input }) => {
       const { email, password } = input;
@@ -22,6 +33,27 @@ const resolvers = {
       } catch (error) {
         console.log(error);
       }
+    },
+    authenticateUser: async (_, { input }) => {
+      const { email, password } = input;
+      const userExists = await User.findOne({ email });
+
+      if (!userExists) {
+        throw new Error("User is not registered.");
+      }
+
+      const correctPassword = await bcryptjs.compare(
+        password,
+        userExists.password
+      );
+
+      if (!correctPassword) {
+        throw new Error("Incorrect Password");
+      }
+
+      return {
+        token: createToken(userExists, process.env.SECRET, "4hr"),
+      };
     },
   },
 };
