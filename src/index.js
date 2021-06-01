dee3f5dcab1b76cde6b0c28d96b144756b5a617b
francijpg <hellofrancijpg@gmail.com>
2021-06-01 08:38:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ const typeDefs = require("./db/schema");
 const resolvers = require("./db/resolvers");
 
 const connectDB = require("./config/db");
+const { token } = require("./utils");
 
 connectDB();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => token.validateToken(req),
+});
 
 server.listen().then(({ url }) => {
   console.log(`Server ready in: ${url}`);
