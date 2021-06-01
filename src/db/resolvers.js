2b302afc67300bc7abd6a22c3dd88668d020664e
francijpg <hellofrancijpg@gmail.com>
2021-06-01 08:37:09 -0400

    Update resolvers.js

--- src/db/resolvers.js
+++ src/db/resolvers.js
@@ -1,64 +1,15 @@
-// const userController = require("../controllers/user");
-const { users } = require("./../controllers");
-
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-require("dotenv").config({ path: ".env" });
-
-const User = require("../models/User");
-
-const createToken = (user, secret, expiresIn) => {
-  const { id, email, name } = user;
-  return jwt.sign({ id, email, name }, secret, { expiresIn });
-};
+const { users, projects } = require("./../controllers");
 
 const resolvers = {
   Query: {
-    getProjects: async (_, {}, ctx) => {
-      console.log("getting projects...");
-    },
+    getProjects: (_, {}, ctx) => projects.getAll(ctx),
   },
   Mutation: {
-    register: (_, { input }) => users.register(input),
-    // createUser: async (_, { input }) => {
-    //   const { email, password } = input;
-    //   const userExists = await User.findOne({ email });
-
-    //   if (userExists) {
-    //     throw new Error("The user is already registered.");
-    //   }
-    //   try {
-    //     const salt = await bcryptjs.genSalt(10);
-    //     input.password = await bcryptjs.hash(password, salt);
-
-    //     const newUser = new User(input);
-    //     newUser.save();
-    //     return "User Created Correctly";
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // },
-    authenticateUser: async (_, { input }) => {
-      // const { email, password } = input;
-      // const userExists = await User.findOne({ email });
-
-      // if (!userExists) {
-      //   throw new Error("User is not registered.");
-      // }
-
-      // const correctPassword = await bcryptjs.compare(
-      //   password,
-      //   userExists.password
-      // );
-
-      // if (!correctPassword) {
-      //   throw new Error("Incorrect Password");
-      // }
-
-      // return {
-      //   token: createToken(userExists, process.env.SECRET, "4hr"),
-      // };
-    },
+    registerUser: (_, { input }) => users.register(input),
+    authenticateUser: (_, { input }) => users.authentication(input),
+    newProject: (_, { input }, ctx) => projects.create(input, ctx),
+    updateProject: (_, { id, input }, ctx) => projects.update(id, input, ctx),
+    deleteProject: (_, { id }, ctx) => projects.remove(id, ctx),
   },
 };
 
