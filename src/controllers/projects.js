b03b29f65f4c94cce0a792e12cc6b20b238d3b3c
francijpg <hellofrancijpg@gmail.com>
2021-06-01 08:36:47 -0400

    Create projects.js

---
+++ src/controllers/projects.js
@@ -0,0 +1,58 @@
+const { Project } = require("../models");
+
+const create = async (input, ctx) => {
+  try {
+    const project = new Project(input);
+    project.creator = ctx.user.id;
+
+    const result = await project.save();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const update = async (id, input, ctx) => {
+  let project = await Project.findById(id);
+
+  if (!project) {
+    throw new Error("Project not found");
+  }
+
+  if (project.creator.toString() !== ctx.user.id) {
+    throw new Error("You don't have the credentials to edit it");
+  }
+
+  project = await Project.findOneAndUpdate({ _id: id }, input, { new: true });
+  return project;
+};
+
+const getAll = async (ctx) => {
+  const projects = await Project.find({ creator: ctx.user.id });
+
+  return projects;
+};
+
+const remove = async (id, ctx) => {
+  let project = await Project.findById(id);
+
+  if (!project) {
+    throw new Error("Project not found");
+  }
+
+  if (project.creator.toString() !== ctx.user.id) {
+    throw new Error("You don't have the credentials to edit it");
+  }
+
+  await Project.findOneAndDelete({ _id: id });
+
+  return "Project Deleted";
+};
+
+module.exports = {
+  create,
+  update,
+  getAll,
+  remove,
+};
