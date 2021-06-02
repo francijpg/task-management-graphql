22e96d1debb60af7496d168e0196209441284587
francijpg <hellofrancijpg@gmail.com>
2021-06-02 11:21:41 -0400

    Update resolvers.js

--- src/db/resolvers.js
+++ src/db/resolvers.js
@@ -1,8 +1,9 @@
-const { users, projects } = require("./../controllers");
+const { users, projects, tasks } = require("./../controllers");
 
 const resolvers = {
   Query: {
     getProjects: (_, {}, ctx) => projects.getAll(ctx),
+    getTasks: (_, { input }, ctx) => tasks.getAll(input, ctx),
   },
   Mutation: {
     registerUser: (_, { input }) => users.register(input),
@@ -10,6 +11,10 @@ const resolvers = {
     newProject: (_, { input }, ctx) => projects.create(input, ctx),
     updateProject: (_, { id, input }, ctx) => projects.update(id, input, ctx),
     deleteProject: (_, { id }, ctx) => projects.remove(id, ctx),
+    newTask: (_, { input }, ctx) => tasks.create(input, ctx),
+    updateTask: (_, { id, input, state }, ctx) =>
+      tasks.update(id, input, state, ctx),
+    deleteTask: (_, { id }, ctx) => tasks.remove(id, ctx),
   },
 };
 
