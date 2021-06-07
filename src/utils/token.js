4cd00366f7f53b28447c92d94df6cfbdb5b14a7c
francijpg <hellofrancijpg@gmail.com>
2021-06-07 07:27:13 -0400

    Update token.js

--- src/utils/token.js
+++ src/utils/token.js
@@ -4,7 +4,7 @@ require("dotenv").config(".env");
 
 const createToken = (user) => {
   const { id, email, name } = user;
-  const secret = process.env.SECRET_KEY;
+  const secret = process.env.JWT_KEY;
   const expiresIn = session.TOKEN_DURATION_TIME;
   return jwt.sign({ id, email, name }, secret, { expiresIn });
 };
@@ -13,7 +13,10 @@ const validateToken = (req) => {
   const token = req.headers["authorization"] || "";
   if (token) {
     try {
-      const user = jwt.verify(token, process.env.SECRET_KEY);
+      const user = jwt.verify(
+        token.replace("Bearer ", ""),
+        process.env.JWT_KEY
+      );
       return {
         user,
       };
